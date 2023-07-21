5044abeff79f6ff85c16bf91a191bde9a1a74a3e
JethroTsai <tsai.jethro@gmail.com>
2023-07-21 12:35:18 +0800

    Added file reading

--- image_enhancer.py
+++ image_enhancer.py
@@ -7,12 +7,21 @@ import multiprocessing
 import time
 
 class image_enhancer(multiprocessing.Process):
-    def __init__(self, edited_loc, new_bright, new_sharp, new_contrast, ctr, queue, remItems):
+    def __init__(self, new_bright, new_sharp, new_contrast):
+        multiprocessing.Process.__init__(self)
+        self.brightness = new_bright
+        self.sharpness = new_sharp
+        self.contrast = new_contrast
+        
+class ImageGetter(multiprocessing.Process):
+    def __init__(self):
         multiprocessing.Process.__init__(self)
 
-class main():
-    photos_loc = input("Location of images: ")
-    edited_loc = input("Location of enhanced images: ")
+def main():
+    #photos_loc = input("Location of images: ")
+    #edited_loc = input("Location of enhanced images: ")
+    photos_loc = "photos"
+    edited_loc = "edited"
     # Enhancing time in minutes
     duration = int(input("input time (in minutes): "))
     # Brightness Enhancement Factor
@@ -23,6 +32,15 @@ class main():
     new_con = float(input("input contrast: "))
     # Optional process count
     process_count = int(input("input process count: "))
+    
+    with multiprocessing.Manager() as manager:
+        image_queue = multiprocessing.Queue()
+        image_sem = multiprocessing.Semaphore(process_count)
+        
+        flist = (listdir(photos_loc))
+        file_list = manager.list(flist)
+        
+        print(flist)
     
 if __name__ == "__main__":
     main() 
