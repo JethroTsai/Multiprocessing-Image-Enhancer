c806aae396be08ba38885f4ed855abbf34b2e189
JethroTsai <tsai.jethro@gmail.com>
2023-07-21 15:11:30 +0800

    Implemented image enhancing

--- image_enhancer.py
+++ image_enhancer.py
@@ -2,64 +2,76 @@ from PIL import Image
 from PIL import ImageEnhance
 import os
 from os import listdir
-import matplotlib.pyplot as plt
-import multiprocessing
 import time
+import multiprocessing
 
-class image_enhancer(multiprocessing.Process):
-    def __init__(self, new_bright, new_sharp, new_contrast):
-        multiprocessing.Process.__init__(self)
-        self.brightness = new_bright
-        self.sharpness = new_sharp
-        self.contrast = new_contrast
-        
-    def enhance(self):
-        # get image and enhance
-        image = ImageEnhance.Brightness(image).enhance(self.brightness)
-        image = ImageEnhance.Sharpness(image).enhance(self.sharpness)
-        image = ImageEnhance.Contrast(image).enhance(self.contrast)
-        
-class ImageGetter(multiprocessing.Process):
-    def __init__(self):
-        multiprocessing.Process.__init__(self)
+class ImageEnhancer:
+    def __init__(self, edited_loc, brightness, sharpness, contrast):
+        self.edited_loc = edited_loc
+        self.brightness = brightness
+        self.sharpness = sharpness
+        self.contrast = contrast
+
+    def enhance_image(self, img_data):
+        imgage, imgage_name = img_data
+        imgage = ImageEnhance.Brightness(imgage).enhance(self.brightness)
+        imgage = ImageEnhance.Sharpness(imgage).enhance(self.sharpness)
+        imgage = ImageEnhance.Contrast(imgage).enhance(self.contrast)
+        imgage.save(os.path.join(self.edited_loc, imgage_name), "JPEG")
+        imgage.show()
+
+def image_enhancer_process(edited_loc, brightness, sharpness, contrast, image_data, counter, remaining_items):
+    enhancer = ImageEnhancer(edited_loc = edited_loc, brightness=brightness, sharpness=sharpness, contrast=contrast)
+    for img_data in image_data:
+        enhancer.enhance_image(img_data)
+        # locks
+        with counter.get_lock():
+            counter.value += 1
+        with remaining_items.get_lock():
+            remaining_items.value -= 1
 
 def main():
-    #photos_loc = input("Location of images: ")
-    #edited_loc = input("Location of enhanced images: ")
-    photos_loc = "photos"
-    edited_loc = "edited"
-    # Enhancing time in minutes
-    duration = int(input("input time (in minutes): "))
-    # Brightness Enhancement Factor
-    new_bri= float(input("input brightness: "))
-    # Sharpness Enhancement Factor
-    new_sha= float (input("input sharpness: "))
-    # Contrast Enhancement Factor
-    new_con = float(input("input contrast: "))
-    # Optional process count
+    photos_loc = input("Location of Images: ")
+    edited_loc = input("Location of Enhanced Images: ")
+    # Enhancing time in units
+    duration = int(input("input time: "))
+    # Brightness
+    brightness = float(input("input brightness: "))
+    # Sharpness
+    sharpness = float(input("input sharpness: "))
+    # Contrast
+    contrast = float(input("input contrast: "))
     process_count = int(input("input process count: "))
-    
+
+    t_start = time.time()
+    # append images
+    image_files = [f for f in listdir(photos_loc) if os.path.isfile(os.path.join(photos_loc, f))]
+    print(image_files)
+
     with multiprocessing.Manager() as manager:
-        time_start = time.time()
-        image_queue = multiprocessing.Queue()
-        image_sem = multiprocessing.Semaphore(process_count)
-        file_sem = multiprocessing.Semaphore(process_count)
-        timer = time.time() + (duration * 60)
-        
-        flist = (listdir(photos_loc))
-        file_list = manager.list(flist)
-        
-        print(flist)
-        while (time.time() < timer):
-            while(len(file_list)):
-                file_sem.acquire()
-                # get files
-                file_sem.release()
-                
-                
-        total_time = time.time() - time_start
-        
-        # write in text file
-    
+        image_data = [(Image.open(os.path.join(photos_loc, fname)), fname) for fname in image_files]
+        counter = multiprocessing.Value('i', 0)
+        remaining_items = multiprocessing.Value('i', len(image_files))
+        # split for each process
+        chunk_size = (len(image_data) // process_count) + 1
+
+        processes = []
+        for i in range(process_count):
+            # calculate start and end index
+            start_idx = i * chunk_size
+            end_idx = min((i + 1) * chunk_size, len(image_data))
+            p = multiprocessing.Process(target=image_enhancer_process,
+                                        args=(edited_loc, brightness, sharpness, contrast,
+                                              image_data[start_idx:end_idx], counter, remaining_items))
+            processes.append(p)
+            p.start()
+
+        # wait for processes
+        for p in processes:
+            p.join()
+
+    t_end = time.time()
+    t_total = t_end - t_start
+
 if __name__ == "__main__":
-    main() 
+    main()
